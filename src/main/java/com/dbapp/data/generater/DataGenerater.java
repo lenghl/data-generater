1814aa5ede43ce55edc7d6d4b003149f3a4c2f37
lenghl <2638497140@qq.com>
2018-11-07 19:04:11 +0800

    init version

---
+++ src/main/java/com/dbapp/data/generater/DataGenerater.java
@@ -0,0 +1,20 @@
+package com.dbapp.data.generater;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 描述:
+ * 数据生成器
+ *
+ * @author lenghl
+ * @create 2018-11-07 16:21
+ */
+public class DataGenerater {
+    private static final Logger logger = LogManager.getLogger(DataGenerater.class);
+
+    public static void main(String[] args) {
+        logger.info("dataGenerater start to run");
+
+    }
+}
